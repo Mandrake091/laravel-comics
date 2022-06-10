40e86a09f052d346b69c55c9847591bb723c2ab2
Paolo Cannistraro <wasdgamer91@gmail.com>
2022-06-10 16:59:51 +0200

    import images, layout complete, scss files, link all files

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,30 @@
+<header>
+    <div class="container">
+        <div class="row">
+            <p>dc power visa</p>
+            <P>additional dc sites</P>
+        </div>
+    </div>
+    <nav>
+    <div class="row">
+      <img src="../images/dc-logo.png" alt="">  
+      <ul>
+        <li><a href="">characters</a></li>
+        <li><a href="">comics</a></li>
+        <li><a href="">movies</a></li>
+        <li><a href="">tv</a></li>
+        <li><a href="">games</a></li>
+        <li><a href="">collectibles</a></li>
+        <li><a href="">videos</a></li>
+        <li><a href="">fans</a></li>
+        <li><a href="">news</a></li>
+        <li><a href="">shop</a></li>
+      </ul>
+      <input type="text">
+    </div>
+</nav>
+
+
+<h1>Ciao!</h1>
+<a href="/comic"><button> vai ad un altra pagina</button></a>
+</header>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $comics = config('comics');
+    return view('home', $comics);
+});
+
+Route::get('/comic', function () {
+    return view('comic');
 });
